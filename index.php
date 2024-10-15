1a758beaf2210b3e35437325fae9f0b5d684f716
alumno pe <alumno pe>
2024-10-15 15:54:06 -0500

    inicio

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo "hola";
+$ruta = "";
+$titulo = "Aplicacion de ventas - Inicio";
+include("Paginas/Includes/cabecera.php");
+?>
+<main>
+    <?php
+    include("Paginas/Includes/menu.php");
+    ?>
+
+    <!-- tabla -->
+    <h1>Productos</h1>
+    <!-- footer -->
+</main>
+<?php
+include("Paginas/Includes/piedepagina.php");
+?>
