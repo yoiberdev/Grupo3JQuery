bf2470f1ac8d00678aa1133e6a92493f5efc381e
Tu Nombre <tu_correo@ejemplo.com>
2024-10-15 14:55:19 -0500

    📖 DOC:

--- view/listar_producto.php
+++ view/listar_producto.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/bootstrap-5.3.3-dist/">
+</head>
 <!-- <?php
     $ruta = "../..";
     $titulo = "aplicacion de ventas - Lista de productos";
