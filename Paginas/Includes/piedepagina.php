59177329795e5fffd125f3d002e0f09c0c2af094
alumno pe <alumno pe>
2024-10-15 16:33:49 -0500

    feat: menu

--- Paginas/Includes/piedepagina.php
+++ Paginas/Includes/piedepagina.php
@@ -1,3 +1,5 @@
 <script src="<?= $ruta; ?>/js/jquery.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
