a988618f83a7e562d5c5c3c14ff6bb1c8e376e05
Tu Nombre <tu_correo@ejemplo.com>
2024-10-15 15:12:32 -0500

    📖 DOC:

---
+++ Paginas/Includes/header.php
@@ -0,0 +1,8 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="../../css/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+</head> -->
